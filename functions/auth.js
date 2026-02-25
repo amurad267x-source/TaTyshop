0cfb3398df2b8bc49d9e5b90991c7701202c5daf
amurad267x-source <amurad267x@gmail.com>
2026-02-25 18:38:23 +0500

    Add files via upload

--- functions/auth.js
+++ functions/auth.js
@@ -1,22 +1,15 @@
-export async function onRequest(context) {
-  const { env } = context;
-
-  const clientId = env.GOOGLE_CLIENT_ID;
-  const redirectUri = env.GOOGLE_REDIRECT_URI;
-
-  const state = crypto.randomUUID();
+export async function onRequest({ env }) {
 
   const googleUrl =
     "https://accounts.google.com/o/oauth2/v2/auth?" +
     new URLSearchParams({
-      client_id: clientId,
-      redirect_uri: redirectUri,
+      client_id: env.GOOGLE_CLIENT_ID.trim(),
+      redirect_uri: env.GOOGLE_REDIRECT_URI.trim(),
       response_type: "code",
       scope: "openid email profile",
-      access_type: "offline",
-      prompt: "consent",
-      state
+      access_type: "online",
+      prompt: "select_account"
     });
 
-  return Response.redirect(googleUrl.toString(), 302);
+  return Response.redirect(googleUrl, 302);
 }
