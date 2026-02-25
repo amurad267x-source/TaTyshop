17bc9ab8a241ff06564211963da7287bb61a7e7d
amurad267x-source <amurad267x@gmail.com>
2026-02-25 18:28:33 +0500

    Add files via upload

---
+++ auth.js
@@ -0,0 +1,15 @@
+export async function onRequest({ env }) {
+
+  const googleUrl =
+    "https://accounts.google.com/o/oauth2/v2/auth?" +
+    new URLSearchParams({
+      client_id: env.GOOGLE_CLIENT_ID.trim(),
+      redirect_uri: env.GOOGLE_REDIRECT_URI.trim(),
+      response_type: "code",
+      scope: "openid email profile",
+      access_type: "offline",
+      prompt: "consent"
+    });
+
+  return Response.redirect(googleUrl, 302);
+}
